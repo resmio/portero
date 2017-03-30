65ba1f8c430d2461d95b2c655f11d187d63db030
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2017-03-30 17:47:20 +0200

    Using heroku db

--- portero/settings.py
+++ portero/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -73,12 +75,15 @@ WSGI_APPLICATION = 'portero.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
+default_db = dj_database_url.config(conn_max_age=500)
+if not default_db:
+    default_db = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+DATABASES = {
+    'default': default_db
 }
 
 
